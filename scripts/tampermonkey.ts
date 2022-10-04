69281646996e8f20c3e4d2847afd4e0073aeba92
Grzegorz Maniak <X00189661@myTUDublin.ie>
2022-10-04 15:51:24 +0100

    Update tampermonkey.ts

--- scripts/tampermonkey.ts
+++ scripts/tampermonkey.ts
@@ -4,13 +4,18 @@ const path = require('path'),
 const tampermonkey_header = `
 // ==UserScript==
 // @name         TUD Moodle Reskin
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/GrzegorzManiak/TUD-Moodle-Reskin
 // @version      1.0.0
 // @description  TUD Tallaght Moodle Reskin 
 // @author       Grzegorz Maniak
 // @match        *://elearning-ta.tudublin.ie/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tudublin.ie
-// @grant        none
+// @run-at       document-start
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getResourceURL
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 `;
 
