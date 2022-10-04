17d07a63346645fae4a4cbd39ed810181e3e762d
Grzegorz Maniak <X00189661@myTUDublin.ie>
2022-10-04 14:08:36 +0100

    Added CSS merging to allow for a cleaner structure

--- scripts/minify-css.ts
+++ scripts/minify-css.ts
@@ -1,16 +1,34 @@
 import { minify } from 'csso';
 import { readFileSync, writeFileSync } from 'fs';
-const path = require('path');   
+const path = require('path'),
+    fs = require('fs');
 
-const PATH = path.resolve(__dirname, '../src/styles.css'),
+const PATH = path.resolve(__dirname, '../src/styles/'),
     OUTPUT = path.resolve(__dirname, '../dist/styles.css');
 
 console.log('Minifying CSS...');
-console.log(PATH + ' -> ' + OUTPUT);    
+console.log('Reading files from ' + PATH);
+console.log('Writing to ' + OUTPUT);
 
-const css = readFileSync(PATH, 'utf8');
+// -- Read all files in the path
+const files = fs.readdirSync(PATH);
 
-const minified = minify(css).css;
+// -- Read all files and minify them
+let css = '';
 
-writeFileSync(OUTPUT, minified, 'utf8');
+files.forEach((file: string) => {
+    const filePath = path.resolve(PATH, file);
 
+    if (fs.statSync(filePath).isFile()) {
+        css += minify(readFileSync(filePath, 'utf8')).css;
+    }
+});
+
+// -- Write the minified css to the output file
+const MINIFIED = minify(css).css;
+
+console.log('Writing ' + MINIFIED.length + ' bytes to ' + OUTPUT);
+
+writeFileSync(OUTPUT, MINIFIED);
+
+console.log('Done!');
